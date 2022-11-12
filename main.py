fe8e1267d227cc13756ed843fce0f30b1cba9ff4
parbhatrai <prabrai11@gmail.com>
2022-11-12 01:17:00 +0000

    continuous loop

--- main.py
+++ main.py
@@ -8,19 +8,18 @@ import time
 import datetime
 
 def chaos_monkey(h, m, s):
-  total_seconds = h*3600 + m*60 + s
+  while True:
+    total_seconds = h*3600 + m*60 + s
+    while total_seconds > 0:
+      time_left = datetime.timedelta(seconds = total_seconds)
 
-  while total_seconds > 0:
-    time_left = datetime.timedelta(seconds = total_seconds)
+      print(time_left)
 
-    print(time_left)
+      time.sleep(1)
 
-    time.sleep(1)
+      total_seconds -= 1
+    delete_pod()
 
-    total_seconds -= 1
-
-  delete_pod()
- 
 # Inputs for hours, minutes, seconds on timer
 h = os.environ.get('h')
 m = os.environ.get('m')
@@ -33,9 +32,7 @@ def delete_pod():
   all_pod = v1.list_namespaced_pod(namespace=ns) #json output
   pod_name = [item.metadata.name for item in all_pod.items] #format in a list
 
-  print(pod_name)
   random_pod = random.choice(pod_name)
-  print(random_pod)
   v1.delete_namespaced_pod(name=random_pod, namespace=ns, body=client.V1DeleteOptions())
   print("Deleting random pod: " + random_pod)
 
