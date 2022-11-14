0c64c8a16ed8da3a91a2a2fda54101684d883e26
parbhatrai <prabrai11@gmail.com>
2022-11-14 13:45:04 +0000

    clean up

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,11 @@ FROM python:3.8-alpine
 LABEL project="chaosmonkey"
 
 WORKDIR /app
+
 RUN adduser --system --no-create-home chaosmonkey
 
 COPY requirements.txt ./
 
-# Install required packages
 RUN pip3 install --no-cache-dir -r requirements.txt
 
 COPY . .
--- main.py
+++ main.py
@@ -5,6 +5,21 @@ import os
 import time
 import datetime
 
+h = os.environ.get('h')
+m = os.environ.get('m')
+s = os.environ.get('s')
+
+def delete_pod():
+  config.load_incluster_config()
+  v1 = client.CoreV1Api()
+  ns = os.environ.get('NAMESPACE')
+  all_pod = v1.list_namespaced_pod(namespace=ns)
+  pod_name = [item.metadata.name for item in all_pod.items]
+
+  random_pod = random.choice(pod_name)
+  v1.delete_namespaced_pod(name=random_pod, namespace=ns, body=client.V1DeleteOptions())
+  print("Deleting random pod: " + random_pod)
+
 def chaos_monkey(h, m, s):
   while True:
     total_seconds = h*3600 + m*60 + s
@@ -14,22 +29,6 @@ def chaos_monkey(h, m, s):
       time.sleep(1)
       total_seconds -= 1
     delete_pod()
-
-# Inputs for hours, minutes, seconds on timer
-h = os.environ.get('h')
-m = os.environ.get('m')
-s = os.environ.get('s')
-
-def delete_pod():
-  config.load_incluster_config()
-  v1 = client.CoreV1Api()
-  ns = os.environ.get('NAMESPACE')
-  all_pod = v1.list_namespaced_pod(namespace=ns) #json output
-  pod_name = [item.metadata.name for item in all_pod.items] #format in a list
-
-  random_pod = random.choice(pod_name)
-  v1.delete_namespaced_pod(name=random_pod, namespace=ns, body=client.V1DeleteOptions())
-  print("Deleting random pod: " + random_pod)
 
 def main():
   chaos_monkey(int(h), int(m), int(s))
